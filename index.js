7cf817769ec7a024d5f7ca435650b5a30feb4797
Fabian Leutgeb <fabian.leutgeb@bitmovin.com>
2020-07-20 19:34:56 +0200

    Use initial throttle rate

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function Daikin(log, config) {
   this.throttle = new Throttle({
     active: true, // set false to pause queue
     rate: 1, // how many requests can be sent every `ratePer`
-    ratePer: 1000, // number of ms in which `rate` requests may be sent
+    ratePer: 500, // number of ms in which `rate` requests may be sent
     concurrent: 1 // how many requests can be sent concurrently
   });
 
