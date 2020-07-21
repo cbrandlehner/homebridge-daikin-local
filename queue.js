3add5ea65f996f3251b154672d92dd754676dd30
Fabian Leutgeb <fabian.leutgeb@bitmovin.com>
2020-07-21 10:43:01 +0200

    Add queue module

---
+++ queue.js
@@ -0,0 +1,29 @@
+function Queue() {
+  this.queue = [];
+  this.running = false;
+}
+
+Queue.prototype.add = function(callback) { 
+    this.queue.push(() => {
+      const next = this.next.bind(this);
+      callback(next);
+    });
+
+    if (!this.running) {
+      this.next();
+    }
+
+    return this;
+}
+
+Queue.prototype.next = function(){
+    this.running = false;
+
+    const shift = this.queue.shift(); 
+    if(shift) { 
+      this.running = true;
+      shift(); 
+    }
+}
+
+module.exports = Queue;
